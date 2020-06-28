cd4636824eabb6d68f509fa66c8be3f57a1b9223
surbhisrivastava28@live.com <surbhisrivastava28@live.com>
2020-06-28 17:07:55 +0530

    Add project files.

---
+++ WebApplication1/Controllers/myController1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ClassLibrary1;
+namespace WebApplication1.Controllers
+{
+    public class MyController1 : ApiController
+    {
+        // GET api/<controller>
+        public IEnumerable<TBL_TRN_TASKS> Get()
+        {
+            using(EasyLivingEntities entities = new EasyLivingEntities())
+            {
+                return entities.TBL_TRN_TASKS.ToList();
+            }
+        }
+
+        // GET api/<controller>/5
+        public TBL_TRN_TASKS Get(int id)
+        {
+            using (EasyLivingEntities entities = new EasyLivingEntities())
+            {
+                return entities.TBL_TRN_TASKS.FirstOrDefault(e => e.TASK_ID == id);
+            }
+        }
+        // POST api/<controller>
+
+    }
+}
